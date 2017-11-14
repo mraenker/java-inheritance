262cbfac8fa3fea1834dd3b3ffe44e4340a94a5c
Martin Raenker <martin.raenker@itomig.de>
2017-11-14 22:25:03 +0100

    BoeseKlasse wegen public und private eingebaut (aber nur fuer Erinnerung)

---
+++ vortrag_vererbung/src/minimalbeispiel/IchBinEineBoeseKlasse.java
@@ -0,0 +1,14 @@
+package minimalbeispiel;
+
+public class IchBinEineBoeseKlasse {
+
+	public static void main (String args[]) {
+		ichBinEineBoeseMethode();
+	}
+	
+	public static void ichBinEineBoeseMethode() {
+		Auto auto1 = new Auto();
+		auto1.bremsen();
+	}
+
+}
